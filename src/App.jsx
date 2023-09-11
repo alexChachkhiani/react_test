44e9dfdc435366622a8ef269ecc4681c058b9500
alexChachkhiani <chachkha@gmail.com>
2023-09-11 17:01:55 +0400

    added some more code

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
 import "./App.css"
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header"
 
 function App() {
+  let number = [1, 2, 3];
+  const myFirstFunction = () => {
+    return 10;
+  } 
   return (
     <div>
-      <h1>Hello</h1>
-      <FirstComponent />
+      <Header />
+      {/* <h1 className="my-first-react-class">Hello React</h1>
+      <p>{number}</p> */}
+      <p>{myFirstFunction()}</p>
+      {/* <FirstComponent /> */}
+      <Footer />
     </div>
   )
 }
 
-function FirstComponent() {
-  return <p>This is my first component</p>
-}
+// function FirstComponent() {
+//   return (
+//   <div>
+//     <p>This is my first component</p>
+//     <footer>
+//       <span>This is my first footer in react</span>
+//     </footer>
+//   </div>
+//   )
+// }
 
 export default App
