54e5e5f1193aaa0121b733f13dfd484f91eac23a
alexChachkhiani <chachkha@gmail.com>
2023-09-11 14:03:06 +0400

    created my first react component

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,13 @@ function App() {
   return (
     <div>
       <h1>Hello</h1>
+      <FirstComponent />
     </div>
   )
+}
+
+function FirstComponent() {
+  return <p>This is my first component</p>
 }
 
 export default App
